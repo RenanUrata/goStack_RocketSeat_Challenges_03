f03d8cbcc8dc1a4708476915796688b1b2569157
Renan Urata <renan.urata@gmail.com>
2020-09-13 08:53:31 -0300

    list feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from './services/api'
 
 function App() {
+  const { repositories, setRepositories } = useState([])
+  const { i, setI } = useState(1)
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
+    const repoNew = {
+      title: `Desafio ${i}`, url: "gostack01.com", techs: "Renan Urata"
+    }
+    const response = await api.post('repositories', repoNew)
+    setRepositories([...repositories, repoNew])
+    setI(i + 1)
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    api.delete(`repositories/${id}`)
+    // const deletedRepo = [...repositories].splice(id, 1)
+    // setRepositories([deletedRepo])
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(
+          repo => (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
           </button>
-        </li>
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
